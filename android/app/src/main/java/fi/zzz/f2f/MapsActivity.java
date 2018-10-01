8240bf94373be5599f9c99584d523663ea52c2ba
Seppo Pakonen <seppo.pakonen@yandex.com>
2018-10-01 21:30:12 +0300

    Android main layout

--- android/app/src/main/java/fi/zzz/f2f/MapsActivity.java
+++ android/app/src/main/java/fi/zzz/f2f/MapsActivity.java
@@ -1,5 +1,6 @@
 package fi.zzz.f2f;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,18 +11,74 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private DrawerLayout mDrawerLayout;
+
+    private static final String TAG = "F2F";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_maps);
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+
+            // Navigation bar
+            mDrawerLayout = findViewById(R.id.drawer_layout);
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(
+                    new NavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(MenuItem menuItem) {
+                            // set item as selected to persist highlight
+                            menuItem.setChecked(true);
+                            // close drawer when item is tapped
+                            mDrawerLayout.closeDrawers();
+                            // Add code here to update the UI based on the item selected
+                            // For example, swap UI fragments here
+                            return true;
+                        }
+                    });
+
+
+            // Set the toolbar as the action bar
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            ActionBar actionbar = getSupportActionBar();
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        catch (java.lang.NullPointerException e) {
+            Log.e(TAG, "System error");
+            System.exit(1);
+        }
     }
 
 
@@ -45,4 +102,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setIndoorEnabled(true);
 
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    // Client code
+    private String addr;
+    private int port = 17000;
+    private Socket sock;
+    private boolean is_logged_in = false;
+    private DataInputStream input;
+    private DataOutputStream output;
+
+    boolean Connect() {
+        if (sock == null || sock.isClosed()) {
+            is_logged_in = false;
+
+            try {
+                sock = new Socket(addr, port);
+
+                input = new DataInputStream(this.sock.getInputStream());
+                output = new DataOutputStream (this.sock.getOutputStream());
+
+                output.writeInt(-1);
+            }
+            catch (UnknownHostException e1) {
+                Log.w(TAG, "Couldn't resolve host");
+                return false;
+            }
+            catch (IOException e) {
+                Log.w(TAG, "Socket IO error");
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
