5059e83dd050a61454b41a25da077b6b6bd1e5ae
Seppo Pakonen <seppo.pakonen@yandex.com>
2018-10-02 23:58:24 +0300

    Client code conversion from C++ version progressed

--- android/app/src/main/java/fi/zzz/f2f/MapsActivity.java
+++ android/app/src/main/java/fi/zzz/f2f/MapsActivity.java
@@ -1,5 +1,12 @@
 package fi.zzz.f2f;
 
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -21,14 +28,152 @@ import android.view.MenuItem;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.URLEncoder;
 import java.net.UnknownHostException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+
+import static java.lang.System.out;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import static java.nio.charset.StandardCharsets.*;
+
+class OpenSslAes {
+
+    /** OpenSSL's magic initial bytes. */
+    private static final String SALTED_STR = "Salted__";
+    private static final byte[] SALTED_MAGIC = SALTED_STR.getBytes();
+
+
+
+    /**
+     *
+     * @param password  The password / key to encrypt with.
+     * @param clearText The data to encrypt
+     * @return  A base64 encoded string containing the encrypted data.
+     */
+    static String encrypt(String password, String clearText) {
+        try {
+            final byte[] pass = password.getBytes();
+            final byte[] salt = (new SecureRandom()).generateSeed(8);
+            final byte[] inBytes = clearText.getBytes();
+
+            final byte[] passAndSalt = array_concat(pass, salt);
+            byte[] hash = new byte[0];
+            byte[] keyAndIv = new byte[0];
+            for (int i = 0; i < 3 && keyAndIv.length < 48; i++) {
+                final byte[] hashData = array_concat(hash, passAndSalt);
+                final MessageDigest md = MessageDigest.getInstance("MD5");
+                hash = md.digest(hashData);
+                keyAndIv = array_concat(keyAndIv, hash);
+            }
+
+            final byte[] keyValue = Arrays.copyOfRange(keyAndIv, 0, 32);
+            final byte[] iv = Arrays.copyOfRange(keyAndIv, 32, 48);
+            final SecretKeySpec key = new SecretKeySpec(keyValue, "AES");
+
+            final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+            byte[] data = cipher.doFinal(inBytes);
+            data = array_concat(array_concat(SALTED_MAGIC, salt), data);
+            return Base64.getEncoder().encodeToString(data);
+        }
+        catch (NoSuchAlgorithmException e) {}
+        catch (InvalidAlgorithmParameterException e) {}
+        catch (BadPaddingException e) {}
+        catch (NoSuchPaddingException e) {}
+        catch (InvalidKeyException e) {}
+        catch (IllegalBlockSizeException e) {}
+        return "";
+    }
+
+    /**
+     * @see http://stackoverflow.com/questions/32508961/java-equivalent-of-an-openssl-aes-cbc-encryption  for what looks like a useful answer.  The not-yet-commons-ssl also has an implementation
+     * @param password
+     * @param source The encrypted data
+     * @return
+     */
+    static String decrypt(String password, String source) {
+        try {
+            final byte[] pass = password.getBytes(US_ASCII);
+
+            final byte[] inBytes = Base64.getDecoder().decode(source);
+
+            final byte[] shouldBeMagic = Arrays.copyOfRange(inBytes, 0, SALTED_MAGIC.length);
+            if (!Arrays.equals(shouldBeMagic, SALTED_MAGIC)) {
+                throw new IllegalArgumentException("Initial bytes from input do not match OpenSSL SALTED_MAGIC salt value.");
+            }
+
+            final byte[] salt = Arrays.copyOfRange(inBytes, SALTED_MAGIC.length, SALTED_MAGIC.length + 8);
+
+            final byte[] passAndSalt = array_concat(pass, salt);
+
+            byte[] hash = new byte[0];
+            byte[] keyAndIv = new byte[0];
+            for (int i = 0; i < 3 && keyAndIv.length < 48; i++) {
+                final byte[] hashData = array_concat(hash, passAndSalt);
+                final MessageDigest md = MessageDigest.getInstance("MD5");
+                hash = md.digest(hashData);
+                keyAndIv = array_concat(keyAndIv, hash);
+            }
+
+            final byte[] keyValue = Arrays.copyOfRange(keyAndIv, 0, 32);
+            final SecretKeySpec key = new SecretKeySpec(keyValue, "AES");
+
+            final byte[] iv = Arrays.copyOfRange(keyAndIv, 32, 48);
+
+            final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+            final byte[] clear = cipher.doFinal(inBytes, 16, inBytes.length - 16);
+            return new String(clear, UTF_8);
+        }
+        catch (NoSuchAlgorithmException e) {}
+        catch (InvalidAlgorithmParameterException e) {}
+        catch (BadPaddingException e) {}
+        catch (NoSuchPaddingException e) {}
+        catch (InvalidKeyException e) {}
+        catch (IllegalBlockSizeException e) {}
+        return "";
+    }
+
+
+    private static byte[] array_concat(final byte[] a, final byte[] b) {
+        final byte[] c = new byte[a.length + b.length];
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+        return c;
+    }
+}
 
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -37,6 +182,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private DrawerLayout mDrawerLayout;
 
     private static final String TAG = "F2F";
+
+    public enum Preferencies {
+        COMPLETED_ONBOARDING_PREF_NAME
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +223,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             ActionBar actionbar = getSupportActionBar();
             actionbar.setDisplayHomeAsUpEnabled(true);
             actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
+            // First time user onboarding activity
+            // https://developer.android.com/training/tv/playback/onboarding
+            /*SharedPreferences sharedPreferences =
+                    PreferenceManager.getDefaultSharedPreferences(this);
+            // Check if we need to display our OnboardingFragment
+            if (!sharedPreferences.getBoolean(
+                    Preferencies.COMPLETED_ONBOARDING_PREF_NAME, false)) {
+                // The user hasn't seen the OnboardingFragment yet, so show it
+                startActivity(new Intent(this, OnboardingActivity.class));
+            }*/
+
         }
         catch (java.lang.NullPointerException e) {
             Log.e(TAG, "System error");
@@ -115,14 +277,59 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
+    class User {
+
+    }
+
+    class Channel {
+
+    }
+
+    class Exc extends Throwable {
+        String msg;
+        Exc(String s) {msg = s;}
+    }
 
     // Client code
+    private int user_id = -1;
+    private String pass;
+    private boolean is_registered = false;
+
+    private Map<String, Channel> channels;
+    private Map<Integer, User> users;
+    private Map<Long, Bitmap> image_cache;
+    private Set<String> my_channels;
+    private String user_name;
     private String addr;
-    private int port = 17000;
     private Socket sock;
+    private int port = 17000;
+    private int age = 0;
+    private boolean gender = false;
     private boolean is_logged_in = false;
     private DataInputStream input;
     private DataOutputStream output;
+    private Lock call_lock, lock;
+
+    void StoreThis() {
+
+    }
+
+    void LoadThis() {
+
+    }
+
+    void RefreshGui() {
+
+    }
+
+    void PostRefreshGui() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                RefreshGui();
+            }
+        });
+    }
 
     boolean Connect() {
         if (sock == null || sock.isClosed()) {
@@ -148,5 +355,246 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
+    boolean LoginScript() throws Exc {
+        if (!is_registered) {
+            try {
+                Register();
+                is_registered = true;
+                StoreThis();
+            }
+            catch (Exc e) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    boolean RegisterScript() throws Exc {
+        if (!is_logged_in) {
+            lock.lock();
+            users.clear();
+            channels.clear();
+            lock.unlock();
+
+            try {
+                Login();
+                RefreshChannellist();
+                RefreshUserlist();
+                RefreshLocation();
+                is_logged_in = true;
+                PostRefreshGui();
+            }
+            catch (Exc e) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void SetName(String s) {
+        if (user_name == s) return;
+        try {
+            if (Set("name", s))
+                user_name = s;
+        }
+        catch (Exc e) {
+            Log.e(TAG, "Changing name failed");
+        }
+    }
+
+    void SetAge(int i) {
+        if (age == i) return;
+        try {
+            if (Set("age", Integer.toString(i)))
+                age = i;
+        }
+        catch (Exc e) {
+            Log.e(TAG, "Changing age failed");
+        }
+    }
+
+    void SetGender(boolean i) {
+        if (gender == i) return;
+        try {
+            if (Set("age", Integer.toString(i ? 1 : 0)))
+                gender = i;
+        }
+        catch (Exc e) {
+            Log.e(TAG, "Changing age failed");
+        }
+    }
+
+    void SetImage(Bitmap i) {
+        long hash = 0;
+        try {
+            String hash_str = Get("profile_image_hash");
+            hash = Long.valueOf(hash_str);
+        }
+        catch (Exc e) {
+            Log.e(TAG, "Getting existing image hash failed");
+        }
+        while (true) {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            i.compress(Bitmap.CompressFormat.JPEG, 80, out);
+            String imgstr = out.toByteArray().toString();
+            if (imgstr.length() > 100000) {
+                int nw = i.getWidth() / 2;
+                int nh = i.getHeight() / 2;
+                i = Bitmap.createScaledBitmap(i, nw, nh, true);
+            } else {
+                if (hash != Hash(imgstr)) {
+                    try {
+                        Set("profile_image", imgstr);
+                    }
+                    catch (Exc e) {
+                        Log.e(TAG, "Changing profile image failed");
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    void StoreImageCache(String image_str) {
+        long hash = Hash(image_str);
+        String img_file = getApplicationContext().getFilesDir() + "/" + Long.toString(hash) + ".bin";
+        try {
+            FileOutputStream fout = new FileOutputStream(img_file);
+            fout.write(image_str.getBytes(), 0, image_str.length());
+            fout.close();
+        }
+        catch (FileNotFoundException f) {
+            Log.e(TAG, "File not found: " + img_file);
+        }
+        catch (IOException f) {
+            Log.e(TAG, "IOException: " + img_file);
+        }
+    }
+
+    boolean HasCachedImage(long hash) {
+        String img_file = getApplicationContext().getFilesDir() + "/" + Long.toString(hash) + ".bin";
+        File f = new File(img_file);
+        return f.exists();
+    }
+
+    String LoadCachedImage(long hash) {
+        String img_file = getApplicationContext().getFilesDir() + "/" + Long.toString(hash) + ".bin";
+        //Bitmap bm = BitmapFactory.decodeFile(img_file);
+        try {
+            FileInputStream fin = new FileInputStream(img_file);
+            return fin.toString();
+        }
+        catch (FileNotFoundException f) {
+            Log.e(TAG, "File not found: " + img_file);
+        }
+        catch (IOException f) {
+            Log.e(TAG, "IOException: " + img_file);
+        }
+        return "";
+    }
+
+    void HandleConnection() {
+        Log.i(TAG, "Client connection running");
+        int count = 0;
+
+        while (!Thread.interrupted()) {
+            Connect();
+
+            try {
+                while (Thread.interrupted() && sock.isConnected()) {
+                    RegisterScript();
+                    LoginScript();
+
+                    Poll();
+                    Sleep(1000);
+                    count++;
+                }
+
+                sock.close();
+            }
+            catch (Exc e) {
+                Log.e(TAG, "Error: " + e);
+            }
+            catch (IOException e) {}
+
+            is_logged_in = false;
+        }
+
+        Log.i(TAG, "Client connection stopped");
+    }
+
+    ByteArrayInputStream Call(OutputStream out) throws Exc {
+        String out_data = OpenSslAes.encrypt("passw0rdpassw0rd", out.toString());
+        String in_data;
+
+        call_lock.lock();
+        try {
+            sock.setSoTimeout(30000);
+            output.writeInt(out_data.length());
+            output.writeBytes(out_data);
+
+            int in_size = input.readInt();
+            byte[] in_buf = new byte[in_size];
+            input.read(in_buf, 0, in_size);
+            in_data = in_buf.toString();
+        }
+        catch (SocketException e) {call_lock.unlock(); throw new Exc("Call: Socket exception");}
+        catch (IOException e) {call_lock.unlock(); throw new Exc("Call: IOException");}
+        call_lock.unlock();
+
+        String dec_data = OpenSslAes.decrypt("passw0rdpassw0rd", in_data);
+        return new ByteArrayInputStream(dec_data.getBytes());
+    }
+
+    void Sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        }
+        catch (InterruptedException e) {}
+    }
+
+    long Hash(String s) {
+        return memhash(s.getBytes(), s.length());
+    }
+
+    long memhash(byte[] ptr, int count) {
+        int hash = 1234567890;
+        for (int i = 0; i < count; i++)
+            hash = ((hash << 5) - hash) ^ ptr[i];
+        return hash & 0x00000000ffffffffL;
+    }
+
+    void Register() throws Exc {
+
+    }
+
+    void Login() throws Exc {
+
+    }
+
+    void RefreshChannellist() {
+
+    }
+
+    void RefreshUserlist() {
+
+    }
+
+    void RefreshLocation() {
+
+    }
+
+    boolean Set(String key, String value) throws Exc {
+
+        return true;
+    }
+
+    String Get(String key) throws Exc {
+
+        return "";
+    }
+
+    void Poll() {
+
+    }
 }
