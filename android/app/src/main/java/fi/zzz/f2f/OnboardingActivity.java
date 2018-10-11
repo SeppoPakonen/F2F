9eb231948f2e4ab08b82c2076100206b9832e01d
Seppo Pakonen <seppo.pakonen@yandex.com>
2018-10-11 16:14:08 +0300

    Added privacy policy to onboarding experience

--- android/app/src/main/java/fi/zzz/f2f/OnboardingActivity.java
+++ android/app/src/main/java/fi/zzz/f2f/OnboardingActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -31,11 +32,53 @@ public class OnboardingActivity extends Activity {
         Button next1 = (Button)findViewById(R.id.next1);
         next1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                SetPage2();
+                SetPage1b();
             }
         });
 
 
+    }
+
+    void SetPage1b() {
+        setContentView(R.layout.onboarding1b);
+
+        Button next1b = (Button)findViewById(R.id.next1b);
+        next1b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SetPage2();
+            }
+        });
+
+        TextView privacypolicy = findViewById(R.id.privacypolicy);
+        String text = "";
+        text = text + "F2F Privacy Policy\n";
+        text = text + "Last updated: 11.10.2018\n";
+        text = text + "\n";
+        text = text + "Team F2F (\"us\", \"we\", or \"our\") operates the android application F2F (the \"Application\"). This page informs you of our policies regarding the collection, use and disclosure of usage data we receive from users of the F2F android application.\n";
+        text = text + "\n";
+        text = text + "We use your Application usage data only for providing and improving the Application. By using the Application, you agree to the collection and use of information in accordance with this policy.\n";
+        text = text + "\n";
+        text = text + "Information Collection And Use\n";
+        text = text + "While using our Application, we don't ask you to provide us any personally identifiable information. We collect your nickname, approximated age, gender and location. The information is used for server usage analysis, which includes generating heatmaps that shows where people have been.\n";
+        text = text + "\n";
+        text = text + "Log Data\n";
+        text = text + "Like many Android application operators, we collect information that your smart device sends whenever you use our Application (\"Log Data\").\n";
+        text = text + "\n";
+        text = text + "This Log Data may include information such as your smart device's Internet Protocol (\"IP\") address, the time and date of your visit, the time spent using the Application and other statistics.\n";
+        text = text + "\n";
+        text = text + "Security\n";
+        text = text + "As we don't collect any personally identifiable information, we do not need to encrypt our traffic according to Play store rules. We do not encrypt the traffic of the Application yet, but it will be a feature of the future version of the Application.\n";
+        text = text + "\n";
+        text = text + "Changes To This Privacy Policy\n";
+        text = text + "This Privacy Policy is effective as of 11.10.2018 and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.\n";
+        text = text + "\n";
+        text = text + "We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. Your continued use of the Application after we post any modifications to the Privacy Policy on this page will constitute your acknowledgment of the modifications and your consent to abide and be bound by the modified Privacy Policy.\n";
+        text = text + "\n";
+        text = text + "If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.\n";
+        text = text + "\n";
+        text = text + "Contact Us\n";
+        text = text + "If you have any questions about this Privacy Policy, please contact us. (email seppo.pakonen@gmail.com)";
+        privacypolicy.setText(text);
     }
 
     void SetPage2() {
